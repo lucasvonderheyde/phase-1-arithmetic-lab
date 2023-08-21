7d2c8dc53e59df6d558c4246e3125799cb8ba636
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-08-21 15:25:44 -0400

    finish lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 19
+const num4 = 5
+
+let max = Math.max(5, 3, 20)
+let mod = num3 % num4
+
